ab725f0965d5edc746522fd1c181579c80486959
Clickady73917 <111101343+Clickady73917@users.noreply.github.com>
2023-02-25 16:59:01 -0800

    Add files via upload

---
+++ spoofall.lua
@@ -0,0 +1,68 @@
+local plr = game.Players.LocalPlayer
+local spoof = true
+local v1 = workspace
+local v2 = v1.MAP
+
+--45_
+--45
+--45
+--45
+--45
+--45
+--45
+
+
+while spoof do
+	local u = "Spoofed Animations"
+	------------------------------------------------------
+	
+	
+	
+	
+	
+	
+	
+	print(u)
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	v2:Destroy()
+end
